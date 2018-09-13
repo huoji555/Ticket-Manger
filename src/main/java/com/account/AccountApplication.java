9d0a46c89632f94aa8e8e2c060ea109e657b4be1
huoji555 <https://github.com/huoji555>
2018-09-13 18:24:42 +0800

    配置不同服务器的环境

--- src/main/java/com/account/AccountApplication.java
+++ src/main/java/com/account/AccountApplication.java
@@ -2,6 +2,7 @@ package com.account;
 
 import javax.servlet.MultipartConfigElement;
 
+import org.apache.commons.collections4.map.HashedMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,19 +12,26 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Map;
+
 @SpringBootApplication
 @EnableCaching                   //开启缓存
 public class AccountApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // 注意这里要指向原先用main方法执行的Application启动类
-        return builder.sources(AccountApplication.class);
-    }
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args
-        );
+
+        SpringApplication application = new SpringApplication(AccountApplication.class);
+        Map<String, Object> defaultMap = new HashedMap<String,Object>();
+        defaultMap.put("pwd", System.getenv("SPWD"));
+        application.setDefaultProperties(defaultMap);
+
+        if (System.getenv("SPWD") == null){
+            SpringApplication.run(AccountApplication.class, args);  //加载线上数据库
+        } else {
+            application.run(args);
+        }
+
     }
 
 
